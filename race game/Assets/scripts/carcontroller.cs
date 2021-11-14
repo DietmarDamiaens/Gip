ea8c56bf036f4bd7e0834627df369e7eb0f4edf0
DietmarDamiaens <ddietmar@hotmail.com>
2021-11-14 10:42:13 +0100

    bewegen gefixt
    rijden werkt nu tegoei, wielen hebbe animatie en we kunne driften

--- race game/Assets/scripts/carcontroller.cs
+++ race game/Assets/scripts/carcontroller.cs
@@ -1,82 +1,78 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class carcontroller : MonoBehaviour
 {
-    public WheelCollider WheelFL;
-    public WheelCollider WheelFR;
-    public WheelCollider WheelRL;
-    public WheelCollider WheelRR;
-    public Transform WheelFLtrans;
-    public Transform WheelFRtrans;
-    public Transform WheelRLtrans;
-    public Transform WheelRRtrans;
-    public Vector3 eulertest;
-    private bool braked = false;
-    private float maxBrakeTorque = 500;
-    private Rigidbody rb;
-    private float maxTorque = 1000;
-    void Start()
+    private float horizontalInput;
+    private float verticalInput;
+    private float steerAngle;
+    private bool isBreaking;
+
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider frontRightWheelCollider;
+    public WheelCollider rearLeftWheelCollider;
+    public WheelCollider rearRightWheelCollider;
+    public Transform frontLeftWheelTransform;
+    public Transform frontRightWheelTransform;
+    public Transform rearLeftWheelTransform;
+    public Transform rearRightWheelTransform;
+
+    public float maxSteeringAngle = 30f;
+    public float motorForce = 10000f;
+    public float brakeForce = 0f;
+
+
+    private void FixedUpdate()
     {
-        rb = GetComponent<Rigidbody>();
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
     }
 
-    void FixedUpdate()
+    private void GetInput()
     {
-        if (!braked)
-        {
-            WheelFL.brakeTorque = 0;
-            WheelFR.brakeTorque = 0;
-            WheelRL.brakeTorque = 0;
-            WheelRR.brakeTorque = 0;
-        }
-        //speed of car, Car will move as you will provide the input to it.
-
-        WheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
-        WheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
-
-        //changing car direction
-        //Here we are changing the steer angle of the front tires of the car so that we can change the car direction.
-        WheelFL.steerAngle = 30 * (Input.GetAxis("Horizontal"));
-        WheelFR.steerAngle = 30 * Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        isBreaking = Input.GetKey(KeyCode.Space);
     }
-    void Update()
+
+    private void HandleSteering()
     {
-        HandBrake();
-
-        //for tire rotate
-        WheelFLtrans.Rotate(WheelFL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
-        WheelFRtrans.Rotate(WheelFR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
-        WheelRLtrans.Rotate(WheelRL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
-        WheelRRtrans.Rotate(WheelRL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
-        //changing tire direction
-        Vector3 temp = WheelFLtrans.localEulerAngles;
-        Vector3 temp1 = WheelFRtrans.localEulerAngles;
-        temp.y = WheelFL.steerAngle - (WheelFLtrans.localEulerAngles.z);
-        WheelFLtrans.localEulerAngles = temp;
-        temp1.y = WheelFR.steerAngle - WheelFRtrans.localEulerAngles.z;
-        WheelFRtrans.localEulerAngles = temp1;
-        eulertest = WheelFLtrans.localEulerAngles;
+        steerAngle = maxSteeringAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = steerAngle;
+        frontRightWheelCollider.steerAngle = steerAngle;
     }
-    void HandBrake()
+
+    private void HandleMotor()
     {
-        //Debug.Log("brakes " + braked);
-        if (Input.GetButton("Jump"))
-        {
-            braked = true;
-        }
-        else
-        {
-            braked = false;
-        }
-        if (braked)
-        {
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
 
-            WheelRL.brakeTorque = maxBrakeTorque * 20;//0000;
-            WheelRR.brakeTorque = maxBrakeTorque * 20;//0000;
-            WheelRL.motorTorque = 0;
-            WheelRR.motorTorque = 0;
-        }
+        brakeForce = isBreaking ? 3000f : 0f;
+        frontLeftWheelCollider.brakeTorque = brakeForce;
+        frontRightWheelCollider.brakeTorque = brakeForce;
+        rearLeftWheelCollider.brakeTorque = brakeForce;
+        rearRightWheelCollider.brakeTorque = brakeForce;
     }
+
+    private void UpdateWheels()
+    {
+        UpdateWheelPos(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateWheelPos(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateWheelPos(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateWheelPos(rearRightWheelCollider, rearRightWheelTransform);
+    }
+
+    private void UpdateWheelPos(WheelCollider wheelCollider, Transform trans)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        trans.rotation = rot;
+        trans.position = pos;
+    }
+
 }
--- race game/Assets/scripts/timescale.cs
+++ race game/Assets/scripts/timescale.cs
@@ -9,7 +9,7 @@ public class timescale : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        modifiedscale = 5;
+        modifiedscale = 1;
     }
 
     // Update is called once per frame
